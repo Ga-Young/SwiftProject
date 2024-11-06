9854c4c4fcb2e6af247677088076ebf18fa088bf
gayoung <rkdudwydkv@gmail.com>
2024-11-06 17:31:14 +0900

    Initial Commit

---
+++ SwiftUIProject/SwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProjectApp.swift
+//  SwiftUIProject
+//
+//  Created by Gayoung Kim on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
