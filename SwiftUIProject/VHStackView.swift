19d218e588472622629f8f23d8849ecf947cfdc1
gayoung <rkdudwydkv@gmail.com>
2024-11-11 15:27:36 +0900

    first commit

---
+++ SwiftUIProject/VHStackView.swift
@@ -0,0 +1,18 @@
+//
+//  VHStackView.swift
+//  SwiftUIProject
+//
+//  Created by Gayoung Kim on 11/6/24.
+//
+
+import SwiftUI
+
+struct VHStackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VHStackView()
+}
